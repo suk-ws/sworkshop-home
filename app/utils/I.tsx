ff48cae1bebf0e224aeebf405fbfccf5641e2d44
Eyre_S <sukazyo@outlook.com>
2024-01-13 11:59:11 +0800

    add more stylus customize
    > notice: the plugin vite-plugin-stylus-alias's original version is
    unavailable in the latest version, so I create an update and gives a pr
    for it. But the npm version need the original author to update so I
    used my local version which can be found at
    https://github.com/Eyre-S/vite-plugin-stylus-alias

--- app/utils/I.tsx
+++ app/utils/I.tsx
@@ -3,7 +3,6 @@ import styles from './i.module.stylus'
 
 import { CSSProperties } from 'react'
 import { classes } from './element'
-import { _$ } from './fp'
 
 interface MaterialSymbolProps {
 	/**
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,18 @@
 import { unstable_vitePlugin as remix } from "@remix-run/dev";
 import { installGlobals } from "@remix-run/node";
 import { defineConfig } from "vite";
-import tsconfigPaths from "vite-tsconfig-paths"
+import vitePluginTsconfigPaths from "vite-tsconfig-paths"
+import vitePluginStylusAlias from "vite-plugin-stylus-alias"
+import path from "node:path"
 
 installGlobals()
+
+const stylusMixins = `
+@import "${path.resolve(__dirname,'./app/styles/mixins')}"
+`
+const stylusConfig = {
+	additionalData: stylusMixins
+}
 
 export default defineConfig({
 	plugins: [
@@ -13,6 +22,18 @@ export default defineConfig({
 				"**/*.stylus"
 			],
 		}),
-		tsconfigPaths()
-	]
+		vitePluginTsconfigPaths(),
+		vitePluginStylusAlias(),
+	],
+	resolve: {
+		alias: {
+			"~": path.resolve(__dirname, "./app"),
+		}
+	},
+	css: {
+		preprocessorOptions: {
+			stylus: stylusConfig,
+			styl: stylusConfig
+		}
+	}
 })
